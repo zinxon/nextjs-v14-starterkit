e76a9211b70d5818f929b8cfb60587c441e2e321
James Wong <pgosnp01@gmail.com>
2024-07-08 23:39:04 -0400

    Title

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -23,8 +23,8 @@ const space_grotesk = Space_Grotesk({
   variable: '--font-space-grotesk'
 })
 export const metadata: Metadata = {
-  title: 'Next Temp',
-  description: 'create next app By Yahya Parvar!'
+  title: 'Wisdomax Realty',
+  description: 'Coming Soon!!'
 }
 
 export default function RootLayout({
